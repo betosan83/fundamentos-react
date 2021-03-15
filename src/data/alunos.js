d4d534bc6964474691150370b898d7883677a010
Roberto <Administrador@NTLD267801.GOV>
2021-03-15 20:01:13 -0300

    Initial commit.

---
+++ src/data/alunos.js
@@ -0,0 +1,10 @@
+export default [
+    { id: 1, nome: 'Ana', nota: 9.2},
+    { id: 2, nome: 'Beto', nota: 8.2},
+    { id: 3, nome: 'Carlos', nota: 9.5},
+    { id: 4, nome: 'Daniel', nota: 3.2},
+    { id: 5, nome: 'Gui', nota: 1.5},
+    { id: 6, nome: 'Pedro', nota: 4.2},
+    { id: 7, nome: 'Oliver', nota: 9.2},
+    { id: 8, nome: 'Cláudia', nota: 9.2}
+]
---
+++ src/data/produtos.js
@@ -0,0 +1,10 @@
+export default [
+    { id: 1, nome: 'Pasta de Dentes', preco: 10.99},
+    { id: 2, nome: 'Café', preco: 11.99},
+    { id: 3, nome: 'Macarrão', preco: 9.99},
+    { id: 4, nome: 'Iogurte', preco: 5.99},
+    { id: 5, nome: 'Pão', preco: 1.99},
+    { id: 6, nome: 'Carne', preco: 99.99},
+    { id: 7, nome: 'Camarão', preco: 50.99},
+    { id: 8, nome: 'Banana', preco: 0.99}
+]
