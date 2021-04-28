1839eac1b872dd82e7cb47cba1feda20b706e2f7
Roberto <Administrador@NTLD267801.GOV>
2021-04-28 07:09:16 -0300

    Contador estilizado

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import  './Contador.css'
+import React, { Component } from 'react'
 
-export default class Contador extends React.Component {
+export default class Contador extends Component {
 
     state = {
-        numero: this.props.numeroInicial
+        numero: this.props.numeroInicial || 0,
+        passo: this.passoInicial || 5
     }
+
+    inc = () => {
+        this.setState({
+            numero: this.state.numero + this.state.passo
+        });
+    }
+    dec = () => {
+        this.setState({
+            numero: this.state.numero - this.state.passo
+        });
+    }
+
+    setPasso = (e) => {
+        this.setState({
+            passo: +e.target.value
+        })
+    }
+
     render() {
         return (
-            <div>
+            <div className="Contador">
                 <h2>Contador</h2>
-                <p>Valor Inicial: {this.state.numero}</p>
+                <h3>{this.state.numero}</h3>
+                <div>
+                    <label htmlFor="passoInput">Passo:</label>
+                    <input id="passoInput" 
+                        type="number" 
+                        value={this.state.passo} 
+                        onChange={this.setPasso} />
+                </div>
+                <button onClick={this.inc}>+</button>
+                <button onClick={this.dec}>-</button>
             </div>
         )
     }
